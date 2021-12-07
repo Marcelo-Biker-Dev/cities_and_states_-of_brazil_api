e690bd4bffdaaddc0b78e802e47195cd58d6cfa3
marcelo.biker.dev@gmail.com <marcelo.biker.dev@gmail.com>
2021-12-07 11:17:28 +0000

    implemented states and cities and performed tests

--- src/main/java/com/github/marcelobikerdev/citiesapi/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.marcelobikerdev.citiesapi.repository;
-
-import com.github.marcelobikerdev.citiesapi.countries.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
